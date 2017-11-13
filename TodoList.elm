83814b31f0a66545d2eaf178fa7deb674c5c9e6f
lucasfrosty <lucas_frosty@hotmail.com>
2017-11-13 06:59:54 -0300

    TodoList added

---
+++ TodoList.elm
@@ -0,0 +1,77 @@
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Style exposing (..)
+
+containerStyle : List Style
+containerStyle =
+  [ margin "5px 5px" ]
+
+inputStyle : List Style
+inputStyle =
+  [ outline "none"
+  , padding "5px"
+  , display "block"
+  , marginBottom "5px"
+  ]
+
+main =
+  beginnerProgram { model = model, view = view, update = update}
+
+-- model
+type alias Model =
+  { todo : String
+  , todos : List String
+  }
+
+model : Model
+model =
+  { todo = ""
+  , todos = []
+  }
+
+
+
+-- view
+todoItem : String -> Html Msg
+todoItem todo =
+  li [] [text todo, button [ onClick (RemoveTodo todo) ] [ text "x" ]]
+
+todoList : List String -> List (Html Msg)
+todoList todos =
+  List.map todoItem todos
+
+view model =
+  div[ style containerStyle ] [
+    input [
+      type_ "text"
+      , style inputStyle
+      , onInput UpdateTodo
+      , value model.todo
+    ] []
+    , button [ onClick AddTodo ] [ text "Submit" ]
+    , button [ onClick RemoveAll ] [ text "Remove All" ]
+    , div [] [ ul [] (todoList model.todos) ]
+  ]
+
+-- update
+type Msg =
+  UpdateTodo String
+  | RemoveTodo String
+  | AddTodo
+  | RemoveAll
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    UpdateTodo text ->
+      { model | todo = text }
+
+    AddTodo ->
+      { model | todos = model.todo :: model.todos }
+    
+    RemoveAll ->
+      { model | todos = [] }
+
+    RemoveTodo text ->
+      { model | todos = List.filter(\y -> y /= text) model.todos }
